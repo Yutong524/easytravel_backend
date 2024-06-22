78a4355e54fd8988177c3e701fdc808dc8ef7de4
Yutong Xia <143774012+Yutong524@users.noreply.github.com>
2024-06-21 22:00:37 -0700

    Initiate UC3 APIs
    just initiate UC3 APIs without implementing the content

---
+++ src/main/java/com/easytravel/easytravel/mdbspringboot/controller/RouteInitiationController.java
@@ -0,0 +1,20 @@
+package com.easytravel.easytravel.mdbspringboot.controller;
+
+import com.easytravel.easytravel.mdbspringboot.model.TravelRoute;
+import com.easytravel.easytravel.mdbspringboot.service.intf.TravelRouteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/travelRoutes")
+@CrossOrigin(origins="http://localhost:3000")
+public class RouteInitiationController {
+    @Autowired
+    private TravelRouteService routeService;
+
+    @PostMapping
+    public String addRoute(@RequestBody TravelRoute route) {
+         
+        return "Route added successfully";
+    }
+}
